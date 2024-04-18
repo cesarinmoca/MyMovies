4f0fc305c4aa5922386b4f931d573d569a052641
cesarinmoca <cesarandree02@gmail.com>
2024-04-18 21:32:03 +0000

    commit

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    # Define tus rutas de URL aquí
+    path("", views.index, name="index"),
+    path("<int:movie_id>/", views.movie_detail, name="movie_detail"),
 ]
