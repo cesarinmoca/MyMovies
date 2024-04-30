25c66f00bcf0307c35ddd197d5420a890d3a3ce1
cesarinmoca <cesarandree02@gmail.com>
2024-04-30 21:49:55 +0000

    commit

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:movie_id>/", views.movie_detail, name="movie_detail"),
-]
+    path("your_name/", views.get_name, name="get_name"),
+    ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from movies.models import Movie
+from .forms import NameForm
 # Create your views here.
 
 
@@ -14,3 +15,24 @@ def movie_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
     context = {'movie': movie}
     return render(request, "movie_detail.html", context=context)
+    
+    
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+    
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+    
+    return render(request, "name.html", {"form": form})
+
+    
