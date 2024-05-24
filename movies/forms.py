3d830a7b75275e263bb749f616cef86e6726e80c
cesarinmoca <cesarandree02@gmail.com>
2024-05-24 05:31:15 +0000

    commit

--- movies/forms.py
+++ movies/forms.py
@@ -1,15 +1,13 @@
 from django import forms
 from .models import MovieReview
 
-
 class NameForm(forms.Form):
     your_name = forms.CharField(label="Nombre", max_length=100, help_text="100 car. máximo", 
                                     error_messages={"required": "El nombre es obligatorio"},
                                     widget=forms.Textarea(attrs={"class":"text-gray-400", "rows": 3, "cols": 60}))
-                                    
+
 class MovieReviewForm(forms.ModelForm):
-    rating = forms.ChoiceField(choices=[(0.5, '0.5'), (1, '1'), (1.5, '1.5'), (2, '2'), (2.5, '2.5'),
-                                         (3, '3'), (3.5, '3.5'), (4, '4'), (4.5, '4.5'), (5, '5')],
+    rating = forms.ChoiceField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')],
                                 label='Rating')
 
     class Meta:
--- movies/models.py
+++ movies/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
-
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
@@ -10,20 +8,17 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-
 class Job(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-
 class Person(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
-
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
@@ -40,16 +35,16 @@ class Movie(models.Model):
     def __str__(self):
         return self.title + ' ' + str(self.release_date.year)
 
-
 class MovieCredit(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
 
-
 class MovieReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
-                                                          MaxValueValidator(100)])
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     review = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.movie.title}"
